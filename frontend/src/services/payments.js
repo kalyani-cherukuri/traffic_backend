059e5d984752417fb042abe9589961623b81af4b
notashock <130152937+notashock@users.noreply.github.com>
2026-05-20 07:14:13 +0530

    fixed payments apii issue

--- frontend/src/services/payments.js
+++ frontend/src/services/payments.js
@@ -1,6 +1,6 @@
 import api from '../api'
 
 export const createPayment = (payload) => api.post('/payments', payload)
-export const getPayments = (params) => api.get('/payments', { params })
+export const getPayments = (params) => api.get('/payments/my', { params })
 
 export default { createPayment, getPayments }
